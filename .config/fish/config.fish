0b8fab0f912097c88f2b39c12491bfe9a3312757
Wojtek Bednarzak <wojtek.bednarzak@gmail.com>
2023-06-12 13:50:01 +0100

    Add more aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,10 +3,30 @@ if status is-interactive
 end
 
 if type -q exa
-  alias ls="exa -l -g --icons"
+  alias ls="exa --long --header --git --icons"
   alias la="ls -a"
 end
 
+if type -q nvim
+  alias vim=nvim
+end
+
+if type -q bat
+  alias ncat=/bin/cat
+  alias cat=bat
+end
+
+if type -q fzf
+  alias fs="fzf --bind 'enter:become(code {})'"
+end
+
+# Setup rtx for application management
 rtx activate fish | source
+
+# GPG setup
+export GPG_TTY=$(tty)
+
+# Kubernetes Aliases
+alias k="kubectl -n"
 
 alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
